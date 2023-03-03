51db82957fdb115f4fac96833dbe9b31c69d8ed1
João Eduardo Seixas <joao.seixas@outlook.pt>
2023-03-03 11:46:35 +0000

    Add Timer for the animation triggers

--- project-hero/Assets/Scripts/Boss.cs
+++ project-hero/Assets/Scripts/Boss.cs
@@ -3,19 +3,32 @@ using UnityEngine;
 public class Boss : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float clawAttackDelay = 5f;  // Time delay for claw attack trigger
+    [SerializeField] private float chompDelay = 10f;      // Time delay for chomp trigger
+    [SerializeField] private Animator animator;          // Reference to the animator component
+
+    private void Start()
     {
-        
+        // Start the activation loop
+        InvokeRepeating("ActivateTriggers", 0f, chompDelay);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ActivateTriggers()
     {
-        
+        // Activate claw attack trigger after delay
+        Invoke("ActivateClawAttackTrigger", clawAttackDelay);
+
+        // Activate chomp trigger after delay
+        Invoke("ActivateChompTrigger", chompDelay);
     }
 
-    private void PerformAttack()
+    private void ActivateClawAttackTrigger()
     {
-        
+        animator.SetTrigger("claw_attack_trigger");
+    }
+
+    private void ActivateChompTrigger()
+    {
+        animator.SetTrigger("chomp_trigger");
     }
 }
